22129503caef600d2a87aadf7fba73f9ccaaf6aa
Chen Bello <ChenBello3@gmail.com>
2024-09-27 16:26:39 +0300

    Unused code is deleted.

--- apiHandling.py
+++ apiHandling.py
@@ -42,32 +42,3 @@ if draw_pokemon.lower() == "y":
     else:
         print("Failed to retrieve Pokémon list.")
 
-# # Ask the user if they would like to draw a Pokémon
-# draw_pokemon = input("Would you like to draw a Pokémon? (y/n) ")
-# if draw_pokemon.lower() == "y":
-#     # If the user wants to draw a Pokémon, make a GET request to the PokeAPI
-#     response = requests.get("https://pokeapi.co/api/v2/pokemon/")
-#     print(response.status_code)
-#
-#     if response.status_code == 200:
-#         # If the request is successful, print the Pokémon's name
-#         pokemon_data = response.json()
-#         print(f"You drew {pokemon_data['name']}")
-#
-#         # Print each ability of the Pokémon
-#         for ability in pokemon_data["abilities"]:
-#             print(ability["ability"]["name"])
-#
-#         # Create lists of ability names, URLs, and whether they are hidden abilities
-#         names = [ability["ability"]["name"] for ability in pokemon_data["abilities"]]
-#         urls = [ability["ability"]["url"] for ability in pokemon_data["abilities"]]
-#         is_hidden = [ability["is_hidden"] for ability in pokemon_data["abilities"]]
-#
-#         # Print the lists
-#         print("Pokémon Abilities:", names)
-#         print("URLs:", urls)
-#         print("Hidden Abilities:", is_hidden)
-#
-#     else:
-#         # If the request fails, print an error message
-#         print("Failed to draw Pokémon")
