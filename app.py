e56289813623a30221a0ced5a8e3810a1f548f51
Chen Bello <ChenBello3@gmail.com>
2024-10-01 16:06:33 +0300

    Trying to fix Errors then changed the code

--- app.py
+++ app.py
@@ -15,4 +15,6 @@ def index():
     return render_template('index.html', pokemon=random_pokemon)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
+
+    # app.run(debug=True)
