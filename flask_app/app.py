9d48a45e3578c36c5b5a38cb4220574d2b458fb0
Chen Bello <ChenBello3@gmail.com>
2024-10-09 17:55:56 +0300

    added GET service '/random_pokemon'.

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,10 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 import json
 import random
 
 app = Flask(__name__)
 
 def get_random_pokemon():
+    # json_path = os.path.join(os.path.dirname(__file__), 'AllPokemons.json')
     with open('AllPokemons.json', 'r') as file:
         pokemons = json.load(file)
         return random.choice(pokemons)
@@ -13,6 +14,11 @@ def get_random_pokemon():
 def index():
     random_pokemon = get_random_pokemon()
     return render_template('index.html', pokemon=random_pokemon)
+
+@app.route('/random_pokemon', methods=['GET'])
+def random_pokemon():
+    pokemon = get_random_pokemon()
+    return jsonify(pokemon)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
